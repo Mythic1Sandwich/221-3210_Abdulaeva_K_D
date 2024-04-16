b54d5f118949edaa4c27c1fba58f93c8b83e8fa4
Mythic1Sandwich <Whatsyourdamage11@gmail.com>
2024-04-16 16:55:46 +0300

    2

--- lab1/mylab/build-app-Desktop_x86_windows_msvc2022_pe_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_app_FILE C:/Users/zarin/mylab/build-app-Desktop_x86_windows_msvc2022_pe_64bit-Debug/app.exe)
-set(__QT_DEPLOY_TARGET_app_RUNTIME_DLLS C:/Qt/6.8.0/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.0/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.8.0/msvc2019_64/bin/Qt6Cored.dll)
